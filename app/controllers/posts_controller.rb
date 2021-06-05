1c6a7f45549d4f759648e0faf32dbe5d5cdc4b6c
Igor.Koloski <koloski_igor@hotmail.com>
2021-06-05 14:23:24 +0200

    Posts add

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+    def new
+        @post = Post.new
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to :root
+        else
+            render :new
+        end
+
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to :root
+        else
+            render :edit
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  has_many :post
   validates :username, presence: true, length: { minimum: 4, maximum: 15}
   validates :mail, presence: true
   validates :password, presence: true, length: { minimum: 6, maximum: 15} 
   
- 
 end
